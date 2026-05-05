6ea02df6508980f3cae8242ce0e9af637c8404cb
Kostiantyn <konstanlebed8@gmail.com>
2026-05-05 11:25:04 +0200

    Add initial server setup with Express and MongoDB connection

---
+++ index.js
@@ -0,0 +1,79 @@
+import express from "express";
+import mongoose from "mongoose";
+import multer from "multer";
+
+import { reqisterValidation } from "./validations/auth.js";
+import { loginValidation } from "./validations/login.js";
+import { postCreateValidation } from "./validations/post.js";
+
+import {checkAuth, handleValidationErrors} from "./utils/index.js";
+
+// import { register, login, getMe } from "./controllers/UserController.js";
+// import {
+//   create,
+//   getAll,
+//   getOne,
+//   remove,
+//   update,
+// } from "./controllers/PostController.js";
+
+import {UserController, PostController} from "./controllers/index.js";
+
+mongoose
+  .connect(
+    "mongodb+srv://konstanlebed8_db_user:jshdsj736jsdjs@cluster1.t4xxrly.mongodb.net/blog?appName=Cluster1",
+  )
+  .then(() => {
+    console.log("DB ok");
+  })
+  .catch((err) => {
+    console.log("DB error", err);
+  });
+
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.post("/auth/login", loginValidation, handleValidationErrors, UserController.login);
+app.post(
+  "/auth/register",
+  reqisterValidation,
+  handleValidationErrors,
+  UserController.register,
+);
+app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+
+app.get("/posts", PostController.getAll);
+app.get("/posts/:id", PostController.getOne);
+app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch("/posts/:id", checkAuth, PostController.update);
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("Server OK");
+});
